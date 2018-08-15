b95faac28977f94c6db21aabaf428b75077a3175
Bessie <33269462+BessieChen@users.noreply.github.com>
2018-08-15 11:55:30 -0400

    Add files via upload

---
+++ week2/strongly_connected/maxers.py
@@ -0,0 +1,54 @@
+# Uses python3
+
+import sys
+sys.setrecursionlimit(200000)
+
+def reverse_graph(adj):
+    reverse_adj = [[] for _ in range(len(adj))]
+    for vertex in range(len(adj)):
+        for adj_vertex in adj[vertex]:
+            reverse_adj[adj_vertex].append(vertex)
+    return reverse_adj
+
+def number_of_strongly_connected_components(adj):
+    result = 0
+    visited = [False] * len(adj)
+    finish_order = []
+    def explore(vertex):
+        visited[vertex] = True
+        for other_vertex in adj[vertex]:
+            if not visited[other_vertex]:
+                explore(other_vertex)
+        finish_order.append(vertex)
+
+    def explore_reverse(vertex):
+        visited[vertex] = True
+        for other_vertex in adj[vertex]:
+            if not visited[other_vertex]:
+                explore(other_vertex)
+
+    for vertex in range(len(adj)):
+        if not visited[vertex]:
+            explore(vertex)
+
+    adj = reverse_graph(adj)
+    visited = [False] * len(adj)
+
+    while finish_order:
+        vertex = finish_order.pop()
+        if not visited[vertex]:
+            explore_reverse(vertex)
+            result += 1
+
+    return result
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    data = list(map(int, input.split()))
+    n, m = data[0:2]
+    data = data[2:]
+    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    adj = [[] for _ in range(n)]
+    for (a, b) in edges:
+        adj[a - 1].append(b - 1)
+    print(number_of_strongly_connected_components(adj))
